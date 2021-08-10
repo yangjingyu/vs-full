72c417ab9afd5a79eaa38ad2030031b8d9271f17
yangjingyu <2836256449@qq.com>
2021-08-10 10:06:25 +0800

    init: 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # full
+
 移动端模拟全屏容器
+
+## 使用
+
+```html
+<div id="full" style="width: 100%;">
+  <button id="toggle">{}</button>
+</div>
+```
+
+```js
+const full = new Full({
+  // 需要全屏的容器
+  el: document.querySelector('#full'),
+  // 切换按钮
+  toggle: document.querySelector('#toggle'),
+});
+```
---
+++ index.js
@@ -0,0 +1,129 @@
+; (function () {
+  function Full(config) {
+    this.el = this.getEl(config.el)
+    this.toggle = this.getEl(config.toggle)
+    this.blank = this.insertBlank()
+    this.bindToggle()
+    this._style = this.el.getAttribute('style')
+
+    this.css = {
+      w: this.el.offsetWidth,
+      h: this.el.offsetHeight,
+    }
+
+    var style = document.createElement("style");
+    style.appendChild(document.createTextNode(".__is_full__{touch-action: none}"));
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(style);
+  }
+
+  Full.prototype.getEl = function (el) {
+    if (typeof el === 'string') {
+      return document.querySelector(el)
+    } else {
+      return el
+    }
+  }
+
+  Full.prototype.insertBlank = function () {
+    var blank = document.createElement('div')
+    var styles = [
+      'position: absolute',
+      'width: 100vw',
+      'height: 100vh',
+      'top: -10000px',
+      'left: -10000px'
+    ].join(';')
+    blank.style = styles
+    document.body.appendChild(blank)
+    return blank
+  }
+
+  Full.prototype.getWH = function () {
+    var data = {
+      ow: this.blank.offsetWidth,
+      oh: this.blank.offsetHeight,
+      ww: window.innerWidth,
+      wh: window.innerHeight,
+      o: window.orientation
+    }
+    data.hasTools = data.h > data.wh
+    data.w = Math.min(data.ow, data.ww)
+    data.h = Math.min(data.oh, data.wh)
+    return data
+  }
+
+  Full.prototype.bindToggle = function () {
+    var that = this
+    this.toggle.addEventListener('click', function () {
+      var body = document.body
+      if (!body.classList.contains('__is_full__')) {
+        body.classList.add('__is_full__')
+        that.getStyle(1)
+        that.__is_full__ = true
+        that.bindOriginChange()
+      } else {
+        body.classList.remove('__is_full__')
+        that.getStyle(2)
+        that.__is_full__ = false
+      }
+    })
+  }
+
+  Full.prototype.getStyle = function (boo) {
+    var that = this
+    var offset = that.getWH()
+    switch (boo) {
+      case 1:
+        var trans = Math.abs(offset.w - offset.h) / 2
+        that.el.style = this._style + [
+          'width:' + offset.h + 'px',
+          'height:' + offset.w + 'px',
+          'position: fixed',
+          'z-index: 99999',
+          'transform:rotate(90deg) translate(' + trans + 'px, ' + trans + 'px)'
+        ].join(';')
+        break
+      case 2:
+        that.el.style = this._style
+        break
+      case 4:
+        that.el.style = this._style + [
+          'width:' + that.css.h + 'px',
+          'height:' + that.css.w + 'px',
+        ].join(';')
+        break
+      case 3:
+        that.el.style = this._style + [
+          'width:' + offset.ww + 'px',
+          'height:' + offset.wh + 'px',
+          'position: fixed',
+          'z-index: 99999',
+        ].join(';')
+        break
+    }
+  }
+
+  Full.prototype.bindOriginChange = function () {
+    var that = this
+    var body = document.body
+
+    var update = function () {
+      if (body.classList.contains('__is_full__')) {
+        if (Math.abs(window.orientation) === 90) {
+          that.getStyle(3)
+        } else {
+          that.getStyle(1)
+        }
+      } else {
+        that.getStyle(2)
+      }
+      if (that.__is_full__) {
+        window.requestAnimationFrame(update)
+      }
+    }
+    window.requestAnimationFrame(update)
+  }
+
+  window.Full = Full
+})();
